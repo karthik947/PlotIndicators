0f1b5edb247ef20c12fc78c7f06f390f7d9e0671
Karthik D <karthik947@gmail.com>
2022-02-19 11:23:01 +0530

    Version 2

--- UI/index.js
+++ UI/index.js
@@ -85,7 +85,7 @@ const renderChart = async () => {
     .filter((d) => d.macd_slow)
     .map((d) => ({ time: d.time, value: d.macd_slow }));
   macd_slow_series.setData(macd_slow_data);
-  //MACD HISTOGRAM
+  //MACD HISTOGRAM //
   const macd_histogram_series = chart.addHistogramSeries({
     pane: 2,
   });
@@ -97,6 +97,64 @@ const renderChart = async () => {
       color: d.macd_histogram > 0 ? 'green' : 'red',
     }));
   macd_histogram_series.setData(macd_histogram_data);
+  //SUPPORT / RESISTANCE LINES
+  const resistanceLine = candleseries.createPriceLine({
+    price: 40400,
+    color: 'red',
+    lineWidth: 2,
+    lineStyle: LightweightCharts.LineStyle.Solid,
+    title: 'Resistance Line',
+  });
+  const supportLine = candleseries.createPriceLine({
+    price: 39800,
+    color: 'green',
+    lineWidth: 2,
+    title: 'Support Line',
+  });
+  //OHLC VALUES
+  chart.subscribeCrosshairMove((param) => {
+    const ohlc = param.seriesPrices.get(candleseries);
+    const rsi = param.seriesPrices.get(rsi_series);
+    renderOHLC(ohlc);
+    renderRSI(rsi);
+  });
+  //CONDITIONAL BACKGROUND
+  const conditional_bgd_series = chart.addHistogramSeries({
+    pane: 0,
+    lineWidth: 0,
+    priceScaleId: 'cpcid1',
+    base: 1,
+  });
+  const conditional_bdg_data = klinedata.map((d, i) =>
+    i < 100
+      ? { time: d.time, value: 0, color: 'rgba(255, 0, 0, 0.1)' }
+      : i < 200
+      ? { time: d.time, value: 0, color: 'rgba(0, 255, 0, 0.1)' }
+      : { time: d.time, value: 0, color: 'rgba(255, 0, 0, 0)' }
+  );
+  conditional_bgd_series.setData(conditional_bdg_data);
+  chart
+    .priceScaleId('cpcid1')
+    .applyOptions({ scaleMargin: { bottom: 0, top: 0 } });
+};
+
+const renderOHLC = (d) => {
+  const { open, high, low, close } = d;
+  const markup = `<p>O<span class="${
+    open > close ? 'red' : 'green'
+  }">${open}</span> H<span class="${
+    open > close ? 'red' : 'green'
+  }">${high}</span> L<span class="${
+    open > close ? 'red' : 'green'
+  }">${low}</span> C<span class="${
+    open > close ? 'red' : 'green'
+  }">${close}</span> </p>`;
+  document.getElementById('ohlc').innerHTML = markup;
+};
+
+const renderRSI = (d) => {
+  const markup = `<p>RSI: ${d.toFixed(2)}</p>`;
+  document.getElementById('rsi').innerHTML = markup;
 };
 
 renderChart();
